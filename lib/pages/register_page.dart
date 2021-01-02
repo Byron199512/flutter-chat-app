20aed2c9d9ddb026eb12fb8d924f93ca2d2b7f2b
Byron Fernando <byron5074@gmail.com>
2021-01-01 19:33:53 -0500

    Login y registro terminado

---
+++ lib/pages/register_page.dart
@@ -0,0 +1,83 @@
+import 'package:chatapp/widgets/btn_azul.dart';
+import 'package:chatapp/widgets/custom_input.dart';
+import 'package:chatapp/widgets/labels.dart';
+import 'package:chatapp/widgets/logo.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class RegisterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color(0xffF2F2F2),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.9,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Logo(
+                    titulo: 'Registro',
+                  ),
+                  _Form(),
+                  Labels(ruta: 'login',titulo: 'Tienes una cuenta?',subTitulo: 'Ingresa ahora!', ),
+                  Text(
+                    'Términos  y condiciones de uso',
+                    style: TextStyle(fontWeight: FontWeight.w200),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
+
+class _Form extends StatefulWidget {
+  @override
+  __FormState createState() => __FormState();
+}
+
+class __FormState extends State<_Form> {
+  final emailCtrl = TextEditingController();
+  final passCtrl = TextEditingController();
+  final nombreCtrl = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 40),
+      padding: EdgeInsets.symmetric(horizontal: 50),
+      child: Column(
+        children: <Widget>[
+          CustomInput(
+            icon: Icons.perm_identity,
+            placeholder: 'Nombre',
+            keyboardType: TextInputType.text,
+            textController: nombreCtrl,
+          ),
+          CustomInput(
+            icon: Icons.mail_outline,
+            placeholder: 'Correo',
+            keyboardType: TextInputType.emailAddress,
+            textController: emailCtrl,
+          ),
+          CustomInput(
+            icon: Icons.lock_outline,
+            placeholder: 'Password',
+            keyboardType: TextInputType.text,
+            textController: passCtrl,
+            ispassword: true,
+          ),
+          BotonAzul(
+              text: 'Crear cuenta',
+              onPressed: () {
+                print(emailCtrl.text);
+                print(passCtrl.text);
+              })
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/usuarios_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class UsuariosPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('hola Usuarios page'),),
+    );
+  }
+}
